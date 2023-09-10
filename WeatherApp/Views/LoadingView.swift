c9643244483bc7b70b090f8f8cc1adc769e343d5
Dion Arya Pamungkas <dionarya.p@gmail.com>
2023-09-10 22:11:50 +0700

    first commit

---
+++ WeatherApp/Views/LoadingView.swift
@@ -0,0 +1,22 @@
+//
+//  LoadingView.swift
+//  WeatherApp
+//
+//  Created by Dion Arya Pamungkas on 10/09/23.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ProgressView()
+            .progressViewStyle(CircularProgressViewStyle(tint: .white))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
