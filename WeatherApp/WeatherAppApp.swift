d712b5853f7fbb8ee2bbfb9dd5e361ad0c26c9bd
Dion Arya Pamungkas <dionarya.p@gmail.com>
2023-09-10 20:32:48 +0700

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Dion Arya Pamungkas on 10/09/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
